bd1c8b15d0b706b57951fd0a32715c54dfc6654f
Det Virak <detvirak5@gmail.com>
2023-09-02 15:30:39 +0700

    change URL

--- src/main/java/com/dvr/sbd/sabay_der_app/config/APIDocConfig.java
+++ src/main/java/com/dvr/sbd/sabay_der_app/config/APIDocConfig.java
@@ -6,19 +6,20 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(info = @Info(contact = @Contact(name = "Det Virak", email = "detvirak5@gmail.com", url = "#"), description = "Version 1 of Sabay Der API", title = "Coffee Shop V1", version = "2.0", license = @License(name = "@SBD", url = "#"), termsOfService = "Terms of service"), servers = {
-        @Server(description = "Local ENV", url = "http://localhost:8080"),
-        @Server(description = "PROD ENV", url = "#")
+@OpenAPIDefinition(info = @Info(contact = @Contact(name = "Det Virak", email = "detvirak5@gmail.com", url = "#"), description = "Version 1 of Sabay Der API", title = "Sabay Der App V1", version = "2.0", license = @License(name = "@SBD", url = "#"), termsOfService = "Terms of service"), servers = {
+                @Server(description = "Local ENV", url = "http://localhost:8080"),
+                @Server(description = "PROD ENV", url = "https://sabay-der-b273e99f1e0a.herokuapp.com")
 }
 
-        , security = {
-
-                @SecurityRequirement(name = "bearerAuth")
-        }
+/*
+ * , security = {
+ * 
+ * @SecurityRequirement(name = "bearerAuth")
+ * }
+ */
 
 )
 @SecurityScheme(name = "Sabay Der", description = "Authorization for Sabay Der Application", scheme = "bearer", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", in = SecuritySchemeIn.HEADER)
